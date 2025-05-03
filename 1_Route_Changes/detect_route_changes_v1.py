16d5834ad5920b420ef12be3f3724cae862648c4
Chengyi <toliu0510@gmail.com>
2025-05-03 19:07:59 -0400

    change packet name for version 3.5.0

--- 1_Route_Changes/detect_route_changes_v1.py
+++ 1_Route_Changes/detect_route_changes_v1.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 from nornir import InitNornir
-from nornir.plugins.tasks.networking import netmiko_send_command
-from nornir.plugins.functions.text import print_result
-
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
 nr = InitNornir()
 
 result = nr.run(task=netmiko_send_command, command_string="show ip route")
